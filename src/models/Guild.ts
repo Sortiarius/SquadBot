818f47afc7f6a03b0acf0c9e0dcc3646cccd4150
Sortiarius <goriastribune@gmail.com>
2020-06-21 01:17:36 -0400

    Prettier

--- src/models/Guild.ts
+++ src/models/Guild.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const Guild = new mongoose.Schema({
-   id: String,
-   prefix: String
+  id: String,
+  prefix: String,
 });
 
 export default mongoose.model("Guild", Guild);
